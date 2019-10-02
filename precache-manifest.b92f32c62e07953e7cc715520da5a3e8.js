38318fda656b34072917ec86c304e4e96c18ceea
nirch <nirh2@yahoo.com>
2019-10-02 22:14:38 +0300

    Updates

---
+++ precache-manifest.b92f32c62e07953e7cc715520da5a3e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ddf85ebc980716a6b9e5b6719779d5",
+    "url": "/recipe-book-test/index.html"
+  },
+  {
+    "revision": "e374edc5cc6138998e44",
+    "url": "/recipe-book-test/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a7e6f59e15fee0edec8b",
+    "url": "/recipe-book-test/static/css/main.0f7e3a42.chunk.css"
+  },
+  {
+    "revision": "e374edc5cc6138998e44",
+    "url": "/recipe-book-test/static/js/2.3e9b5f8b.chunk.js"
+  },
+  {
+    "revision": "a7e6f59e15fee0edec8b",
+    "url": "/recipe-book-test/static/js/main.d7003161.chunk.js"
+  },
+  {
+    "revision": "332ae46814a68e9d4ad0",
+    "url": "/recipe-book-test/static/js/runtime-main.1839a1e6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/recipe-book-test/static/media/logo.25bf045c.svg"
+  }
+]);
