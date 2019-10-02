6d7dff3816ff241b229b994e82a4d02417990117
nirch <nirh2@yahoo.com>
2019-10-02 13:06:47 +0300

    RecipeNavbar component

--- src/App.js
+++ src/App.js
@@ -5,34 +5,22 @@ import './App.css';
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+import RecipeNavbar from "./components/RecipeNavbar"
 
+class App extends React.Component {
 
-function App() {
-
-  return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/#/">Recipe Book</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Recipes</Nav.Link>
-          </Nav>
-          <Nav className="ml-auto">
-            <Nav.Link href="#/signup">Signup</Nav.Link>
-            <Nav.Link href="#/login">Login</Nav.Link>
-            <Nav.Link href="#/logout">Logout</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/login" component={LoginPage}/>
-      {/* <Route path="/recipes" component={RecipesPage}/> */}
-      </Switch>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <RecipeNavbar/>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          {/* <Route path="/recipes" component={RecipesPage}/> */}
+        </Switch>
+      </div>
+    );
+  }
 
 }
 
