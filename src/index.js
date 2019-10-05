531b307d692407e9198f5f6e4b6836ff3e7e93dd
nirch <nirh2@yahoo.com>
2019-10-05 20:14:55 +0300

    adding parse library + initialization

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter} from 'react-router-dom'
+import Parse from 'parse'
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+Parse.initialize(
+    'bPiipOdH63i5L5i8CnVJNv72hwa8ZLWsvMDxUuMN', // This is your Application ID
+    'tBsVpAtPAzqy8oJ6cfwj51lWrApdJt5I0KmGkOYo' // This is your Javascript key
+  );
 
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
